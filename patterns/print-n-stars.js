9a30b45ca47d4e86e821c0e240d9dbba6debff8d
Akhilesh <makmishra.99@gmail.com>
2024-02-06 16:55:34 +0530

    print-n-stars

---
+++ patterns/print-n-stars.js
@@ -0,0 +1,17 @@
+/* n starts to be printed for n rows */
+
+/* if any either of left or right operator is string , then it will convert another one as string and joins them */
+/* in case of "+" operators */
+
+/* this is also know coersion */
+function printNStars(n) {
+  for (let row = 1; row <= n; row++) {
+    let stars = "";
+    for (let col = 1; col <= n; col++) {
+      stars += "*";
+    }
+    console.log(stars);
+  }
+}
+
+printNStars(10);
