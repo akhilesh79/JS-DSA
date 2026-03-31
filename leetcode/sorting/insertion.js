bd348d158c80345b81fe6471ca0c4350249af6ee
Akhilesh Kumar Mishra <makmishra.99@gmail.com>
2026-03-31 21:52:19 +0530

    sorting - algorithm

---
+++ leetcode/sorting/insertion.js
@@ -0,0 +1,18 @@
+let arr = [4, 7, 3, 1, 2, 5];
+
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let curr = arr[i];
+    let prev = i - 1;
+    while (arr[prev] > curr && prev >= 0) {
+      arr[prev + 1] = arr[prev];
+      prev--;
+    }
+
+    arr[prev + 1] = curr;
+  }
+
+  return arr;
+};
+
+console.log(insertionSort(arr));
